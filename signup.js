11d807ed0da77cd6613b56057e332940302d93ab
olatunji0001 <musbautunji@gmail>
2025-12-13 22:55:11 +0100

    new update

--- signup.js
+++ signup.js
@@ -22,7 +22,7 @@ export const sigup = async (req, res) => {
           });
         }
       } catch (error) {
-        console.log(error);
+        console.log(error.message);
       }
     }
   } catch (error) {
